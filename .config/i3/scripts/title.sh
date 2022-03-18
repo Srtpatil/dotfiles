9ab3798dc1a03093f9faf0b229e82e7c2ae9f935
Samarth Patil <samarthpatil15@gmail.com>
2022-03-18 08:09:43 +0000

    test

--- .config/i3/scripts/title.sh
+++ .config/i3/scripts/title.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-i3-msg -t subscribe -m '[ "window" ]'| jq --unbuffered -Mrc '. | select(.container.focused == true).container.window_properties.title'|| true |
+i3-msg -t subscribe -m '[ "window" ]'| jq --unbuffered -MrcR 'fromjson? | . | select(.container.focused == true).container.window_properties.title' | 
 while IFS= read -r title; do
     # echo "$title" | cut -c -50
 		echo $title | awk '{print substr($0, 1, 50) "..."}'
